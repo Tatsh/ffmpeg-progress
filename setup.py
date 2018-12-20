4ffa70d57f101eaae9dfb4edb9f8ef483cca67eb
Andrew Udvare <audvare@gmail.com>
2018-12-20 10:08:52 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='ffmpeg-progress',
-      version='0.0.1',
+      version='0.0.2',
       author='Andrew Udvare',
       author_email='audvare@gmail.com',
       url='https://github.com/Tatsh/ffmpeg-progress',
